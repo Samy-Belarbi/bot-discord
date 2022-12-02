fda00929dc722fa7c58bc7c381badbacabdac0b6
Samy <samy.belarbi@3wa.io>
2022-12-02 10:37:49 +0100

    first commit

--- Commands/meet.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-  name: "meet",
-  async run(bot, message) {
-    await message.reply(`@here Connecte toi vite : ••••••••••••••••••••`);
-  },
-};
--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  token:
-    "MTA0NTAxNzM1MDI5NjA0Mzc1MA.GM7VLh.9uSZtmtGy2duedqTHuGPVKng4lO0MaO_2EAHAg",
-};
